06f9eb59dfd2505c544c804d8b287149ffc0b01e
jaekeun Choi <jaek1783@gmail.com>
2023-05-31 22:47:52 +0900

    키보드 이벤트 추가로 블럭조작하기

--- components/game-blocks.tsx
+++ components/game-blocks.tsx
@@ -8,14 +8,69 @@ const Blocks = ({num, matrix, MovingItem})=>{
 useEffect(()=>{
     const renderBlocks = ()=>{
         const {type, direction, top, left} = tempMovingItem;
+        const movingBlocks = document.querySelectorAll('.moving');
+        movingBlocks.forEach(moving=>{
+            moving.classList.remove(type, 'moving');
+        })
         BlockItems[type][direction].forEach(block => {
-            const x = block[0];
-            const y = block[1];
-            const target = blocksRef.current.childNodes[y].childNodes[0].childNodes[x];
-            target.classList.add(type)
+            const x = block[0]+left;
+            const y = block[1]+top;
+            const target = 
+            blocksRef.current.childNodes[y]? 
+            blocksRef.current.childNodes[y].childNodes[0].childNodes[x] :
+            null;
+            target ? target.classList.add(type, "moving") : 
+            tempMovingItem = {...MovingItem}
+            setTimeout(()=>{
+                renderBlocks()
+            },0);
         });
+    };
+
+    const moveBlock = (moveType, amount) =>{
+        console.log(tempMovingItem);
+        if (!tempMovingItem) {
+            tempMovingItem = {
+            left:0,
+            top: 0,
+            }
+        }
+        if(tempMovingItem){
+            tempMovingItem[moveType] += amount;
+        }
+        renderBlocks();
     }
-    return renderBlocks();
+// event.handling 키보드 작동
+const handleKeyDown = (e) => {
+    switch (e.keyCode) {
+      case 39:
+        moveBlock("left", 1);
+        break;
+      case 37:
+        moveBlock("left", -1);
+        break;
+      case 40:
+        moveBlock("top", 1);
+        break;
+      //   case 38:
+      //     changeDirection();
+      //     break;
+      //   case 32:
+      //     dropBlock();
+      //     break;
+      default:
+        break;
+    }
+  };
+
+  // 이벤트 리스너 추가
+  document.addEventListener("keydown", handleKeyDown);
+
+  // 정리 함수
+  return () => {
+    // 이벤트 리스너 제거
+    document.removeEventListener("keydown", handleKeyDown);
+  };
 },[]);
 
     const tableLoop = ()=>{
